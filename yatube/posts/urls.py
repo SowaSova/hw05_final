b3704a333078ccda1e91aee5dec0ee134842cb79
Алексей Николаев <cei4bio@gmail.com>
2022-06-11 20:48:07 +0300

    6th.1st.

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -18,10 +18,10 @@ urlpatterns = [
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
 
     path(
-        'posts/<int:post_id>/comment/', 
-        views.add_comment, 
+        'posts/<int:post_id>/comment/',
+        views.add_comment,
         name='add_comment'
-        ),
+    ),
 
     path('follow/', views.follow_index, name='follow_index'),
 
